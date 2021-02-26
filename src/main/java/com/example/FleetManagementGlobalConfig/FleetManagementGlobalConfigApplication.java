c61c9283bda6d1aa7b2c29d769d3a86492d108d4
kristinau09 <kristinau09@gmail.com>
2021-02-25 18:20:41 -0600

    Global Config for Fleet management of vehicles

---
+++ src/main/java/com/example/FleetManagementGlobalConfig/FleetManagementGlobalConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.FleetManagementGlobalConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementGlobalConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementGlobalConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FleetManagementGlobalConfig/FleetManagementGlobalConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FleetManagementGlobalConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementGlobalConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
